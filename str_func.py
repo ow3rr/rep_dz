2e56f25f2a38ad9f0d3c51e3373d464f6f71e4ab
ow3rr <lighteckiy@ya.ru>
2023-09-13 19:27:07 +0300

    add func capitalize_string

---
+++ str_func.py
@@ -0,0 +1,7 @@
+def capitalize_string(s):
+    return s.upper()
+
+
+string = input("Введите строку: ")
+capital_words = capitalize_string(string)
+print(f"Строка в заглавных буквах: {capital_words}")
